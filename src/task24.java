9ac2ce46288d782eecf59714cb5a3b7fae4b3995
Mawadda Alshukaili <moodalshukaili99@gmail.com>
2026-04-08 17:02:19 +0400

    Silving task24

---
+++ src/task24.java
@@ -0,0 +1,18 @@
+public class task24 {
+    public static void main(String[] args) {
+        int[] nums1 = {1, 2, 2, 3};
+        System.out.println(has22(nums1));
+    }
+
+    public static boolean has22(int[] nums) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == 2 && nums[i + 1] == 2) {
+                return true;
+            }
+        }
+            return false;
+        }
+
+
+    }
